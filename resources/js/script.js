4663da0a0cf4018414a7146ee59e97080e3e2d4c
Daniel Matook <danielmatook@gmail.com>
2019-10-27 04:22:05 -0700

    Jquery edit for title animation past first panel

--- resources/js/script.js
+++ resources/js/script.js
@@ -11,11 +11,4 @@ $(document).ready(function() {
             offset: '60px;'
     });
 
-    // var waypoints = $('#handler-first').waypoint(function(direction) {
-    //     notify(this.element.id + ' hit 25% from top of window') 
-    //   }, {
-    //     offset: '25%'
-    //   })
-
-
 });
